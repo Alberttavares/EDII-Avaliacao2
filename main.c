93b680ae35695dd392436247a0d4c93d578a9558
Alberttavares <gerenciamentoalberttavares@gmail.com>
2026-06-25 21:27:14 -0300

    ✨feat: Add initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+/* 
+==============================================
+|     Trabalho de Estrutura de Dados II      |
+----------------------------------------------
+|                 Alunos                     |
+----------------------------------------------
+| Gabriel Vargas de Melo                     |
+| Isaac Gabriel Covre Silva                  |
+| Albert Rocha Tavares                       |
+| Victor Rodrigues Silva                     |
+============================================== 
+*/
